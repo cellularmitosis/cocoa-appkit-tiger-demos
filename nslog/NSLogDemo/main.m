4098e426bbbd07bb36edf7cb53e27b170a374412
Jason Pepas <jasonpepas@gmail.com>
2023-03-11 09:45:21 -0600

    NSLog demo

---
+++ nslog/NSLogDemo/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSLogDemo
+//
+//  Created by Jason Pepas on 3/11/23.
+//  Copyright __MyCompanyName__ 2023. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	NSLog(@"Hello, world!");
+    return NSApplicationMain(argc,  (const char **) argv);
+}
---
+++ nslog/README.md
@@ -0,0 +1,13 @@
+# NSLog
+
+Trivial example of using NSLog.
+
+```objc
+int main(int argc, char *argv[])
+{
+        NSLog(@"Hello, world!");
+            return NSApplicationMain(argc,  (const char **) argv);
+}
+```
+
+![](media/nslog-demo.png)
