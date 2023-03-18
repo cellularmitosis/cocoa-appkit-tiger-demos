f931267d792e0e8871bf52497fda09338a814c1e
Jason Pepas <jasonpepas@gmail.com>
2023-03-18 18:00:52 -0500

    use argv[1] as the URL

--- nsurlconnection-sync-no-xcode/main.m
+++ nsurlconnection-sync-no-xcode/main.m
@@ -1,27 +1,36 @@
 #import <Foundation/Foundation.h>
+#include <stdio.h>
+
+// GET the contents of an HTTP URL and print it to stdout.
 
 int main(int argc, char** argv) {
     NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
 
-    NSURL* url = [NSURL URLWithString:@"http://leopard.sh"];
+	NSString* urlString = @"http://leopard.sh/README.txt";
+    if (argc > 1) {
+    	urlString = [NSString stringWithCString:argv[1]
+								 encoding:NSASCIIStringEncoding];
+    }
+    NSURL* url = [NSURL URLWithString:urlString];
     NSURLRequest* request = [NSURLRequest requestWithURL:url];
 
     NSURLResponse* response = nil;
     NSError* error = nil;
-    NSData* data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-    NSLog(@"sent request");
+    NSData* data = [NSURLConnection sendSynchronousRequest:request
+										 returningResponse:&response
+													 error:&error];
 
-    int ret = 0;
+    int exitStatus = 0;
     if (error != nil) {
         NSLog(@"Error: %@", error);
-        ret = 1;
+        exitStatus = 1;
     } else if (data != nil) {
-        NSLog(@"Retrieved %u bytes of data:", [data length]);
-        NSString* string = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
-        [string autorelease];
-        NSLog(@"%@", string);
+        NSString* string = [[NSString alloc] initWithData:data
+												 encoding:NSASCIIStringEncoding];
+        printf([string UTF8String]);
+        [string release];
     }
 
     [pool release];
-    return ret;
+    return exitStatus;
 }
